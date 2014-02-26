2a45fe673a9f243e56d54d28d430f50b66911a4c
Simon Busch <morphis@gravedo.de>
2014-02-26 08:41:46 +0100

    Disable building of java module
    Signed-off-by: Simon Busch <morphis@gravedo.de>

--- Android.mk
+++ Android.mk
@@ -24,13 +24,3 @@ LOCAL_MODULE := liblzf
 LOCAL_SRC_FILES := $(libLZF_SRC_FILES)
 
 include $(BUILD_STATIC_LIBRARY)
-
-# JAR for host
-# ========================================================
-include $(CLEAR_VARS)
-
-LOCAL_SRC_FILES := $(call all-java-files-under, src)
-LOCAL_MODULE := liblzf
-LOCAL_MODULE_TAGS := optional
-
-include $(BUILD_HOST_JAVA_LIBRARY)
